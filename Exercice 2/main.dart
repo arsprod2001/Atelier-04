b599504ebf32b16dbbc2ef82d1e7d2005ce5b60a
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-10-16 10:06:09 -0400

    Exo2 : Ajout du méthode display

--- Exercice 2/main.dart
+++ Exercice 2/main.dart
@@ -1,5 +1,17 @@
-class Point{
+class Point {
   double _x, _y;
   Point(this._x, this._y);
-  
+
+  void move(int dx, int dy) {
+    _x += dx;
+    _y += dy;
+
+    print("Mouvement réussi");
+  }
+
+  //double get x => _x;
+  //double get y => _y;
+  String display() {
+    return "X = $_x, Y = $_y";
+  }
 }
