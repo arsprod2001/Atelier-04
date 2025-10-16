13154138f2cd753b6a5a6dba817416253d19982f
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-10-16 10:10:38 -0400

    Exo2 : Fixing : Adding Getter and Setter

--- Exercice 2/main.dart
+++ Exercice 2/main.dart
@@ -2,15 +2,15 @@ class Point {
   double _x, _y;
   Point(this._x, this._y);
 
-  void move(int dx, int dy) {
+  double get x => _x;
+  double get y => _y;
+
+  set move(int dx,int dy) {
     _x += dx;
     _y += dy;
-
     print("Mouvement réussi");
   }
 
-  //double get x => _x;
-  //double get y => _y;
   String display() {
     return "X = $_x, Y = $_y";
   }
