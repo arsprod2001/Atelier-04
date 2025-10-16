c41844433432b5c276e6d5350730fa262c9a3e75
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-10-16 09:54:00 -0400

    implementation du fonction main

--- Exercice 01/main.dart
+++ Exercice 01/main.dart
@@ -5,7 +5,22 @@ class Couleur {
   Couleur.vert() : r = 0, g = 255, b = 0;
   Couleur.bleu() : r = 0, g = 0, b = 255;
 
-  void AfficheCouleur (){
-    print("R : $r, G : $g, B: $b");
+  String AfficheCouleur (){
+    return "R : $r, G : $g, B: $b";
   }
+}
+
+void main(){
+  var c1 = Couleur(253, 5, 7);
+  print(c1.AfficheCouleur()); 
+
+  Couleur r1 = Couleur.rouge();
+  print(r1.AfficheCouleur());
+
+  Couleur g1 = Couleur.vert();
+  print(g1.AfficheCouleur());
+
+  Couleur b1 = Couleur.vert();
+  print(b1.AfficheCouleur());
+
 }
