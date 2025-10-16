16c588c8bcabb9ee249bc45a1ef3df7b398d33b3
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-10-16 09:45:00 -0400

    Création du methode publique AfficherCouleur

--- Exercice 01/main.dart
+++ Exercice 01/main.dart
@@ -1,4 +1,11 @@
 class Couleur {
   final int r, g, b;
   Couleur(this.r, this.g, this.b);
+  Couleur.rouge() : r = 255, g = 0, b = 0;
+  Couleur.vert() : r = 0, g = 255, b = 0;
+  Couleur.bleu() : r = 0, g = 0, b = 255;
+
+  void AfficheCouleur (){
+    print("R : $r, G : $g, B: $b");
+  }
 }
