fb5cdebf89346633cd21652d0e02ebb005428d99
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-10-29 22:05:04 -0400

    Exo 3 : Implementation du methode publique depot

--- Exercice 03/main.dart
+++ Exercice 03/main.dart
@@ -5,4 +5,13 @@ class Compte{
   Compte(this.numeroCompte, [this._solde = 0.0]);
   String get solde => "${_solde.toStringAsFixed(2)} \$";
 
+  void depot(double montant){
+    if(montant > 0){
+      _solde += montant;
+      print("Dépot de ${montant.toStringAsFixed(2)} \$ reussi");
+
+    } else {
+      print("Le montant du depot doit etre positif");
+    }
+  }
 }
