f696cc60bb3df9ea96ee81527045033b8255e2c0
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-10-29 22:38:22 -0400

    Exo 5 : Methode calculer aire qui returne l'aire du cercle

--- Exercice 05/main.dart
+++ Exercice 05/main.dart
@@ -1,7 +1,19 @@
+import 'dart:math';
+
 abstract class Forme {
   double calculerAire();
 
   void afficherMessage(){
     print("Calcul de l'air");
+  }
+}
+
+class Cercle extends Forme{
+  final double rayon;
+  Cercle(this.rayon);
+
+  @override
+  double calculerAire(){
+    return pi * pow(rayon, 2);
   }
 }
